ed650bc55fb48ee4c0cfed0d1798c319233b4a41
Jesse Zhu <ITJesse@users.noreply.github.com>
2016-11-14 05:03:24 +0900

    Update start.sh

--- assets/start.sh
+++ assets/start.sh
@@ -8,5 +8,6 @@ sed 's/{{server}}/'"${SERVER_IP}"'/' -i /etc/shadowsocks-libev/config.json
 sed 's/{{local}}/'"${LOCAL_IP}"'/' -i /etc/shadowsocks-libev/config.json
 sed 's/{{password}}/'"${SERVER_PASSWORD}"'/' -i /etc/shadowsocks-libev/config.json
 sed 's/{{method}}/'"${SERVER_METHOD}"'/' -i /etc/shadowsocks-libev/config.json
+sed 's/{{timeout}}/'"${TIMEOUT}"'/' -i /etc/shadowsocks-libev/config.json
 
 exec /usr/bin/supervisord
