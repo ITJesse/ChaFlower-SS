2604ccbdd7122e17040966ddbc1b08164c09becf
Jesse Zhu <ITJesse@users.noreply.github.com>
2016-11-14 05:04:02 +0900

    Update start.sh

--- assets/start.sh
+++ assets/start.sh
@@ -3,6 +3,8 @@
 SERVER_IP=${SERVER_IP:-0.0.0.0}
 SERVER_PASSWORD=${SERVER_PASSWORD}
 SERVER_METHOD=${SERVER_METHOD}
+LOCAL_IP=${LOCAL_IP:-0.0.0.0}
+TIMEOUT=${TIMEOUT:-600}
 
 sed 's/{{server}}/'"${SERVER_IP}"'/' -i /etc/shadowsocks-libev/config.json
 sed 's/{{local}}/'"${LOCAL_IP}"'/' -i /etc/shadowsocks-libev/config.json
